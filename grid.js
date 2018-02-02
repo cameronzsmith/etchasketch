32ffd996d8b07f77bbff3de6131e0cd1d9f2330d
Cameron Smith <csmith158@butlercc.edu>
2018-02-01 21:32:20 -0600

    Added rainbow option, styled page further

--- grid.js
+++ grid.js
@@ -1,5 +1,9 @@
 createGrid(16); // Creates a blank 16x16 grid.
-_resetButtonClicked();
+_generateClickHandler(); // Add generate button click event listener.
+_clearClickHandler();
+_rainbowClickHandler();
+
+let rainbowActivated = false;
 
 // Creates a blank grid
 function createGrid(cell_size) {
@@ -30,17 +34,61 @@ function createGrid(cell_size) {
 // Adds hover effect event listener for each cell.
 function setActive(cell) {
     cell.addEventListener("mouseover", function() {
-        cell.classList.add("active");
+        if(rainbowActivated) {
+            cell.classList.remove("active");
+            cell.style = `background: rgb(${generateRGB()}, ${generateRGB()}, ${generateRGB()}`; // Generates a random color for the cells
+        } else {
+            cell.style.background = "#fff";
+            cell.classList.add("active");
+        }
     })
 }
 
-function _resetButtonClicked() {
-    let resetButton = document.querySelector(".resetButton");
+// Generates a random number between between 0 and 254.
+function generateRGB() {
+    return Math.floor(Math.random() * 255);
+}
+
+// Destroys the old grid and creates a new one based on user input.
+function _generateClickHandler() {
+    let resetButton = document.querySelector(".reset");
 
     resetButton.addEventListener("click", function() {
-        let cellSize = prompt("Cell size", 16);    
+        let cellSize = prompt("How many rows by columns?", 16);    
         deleteGrid();
         createGrid(cellSize);
+    })
+}
+
+// Clears the grid of all active/rainbow elements.
+function _clearClickHandler() {
+    let clearButton = document.querySelector(".clear");
+
+    clearButton.addEventListener("click", function() {
+
+        document.querySelectorAll(".cell").forEach(function(element) {
+            element.classList.remove("active");
+            element.style.background = "#393E41";
+        });
+
+    });
+}
+
+// Checks if rainbow mode is activated, and if it is, stylize the background.
+function _rainbowClickHandler() {
+    let rainbowButton = document.querySelector(".rainbow");
+    let rainbowOldBG = rainbowButton.style.background;
+
+    rainbowButton.addEventListener("click", function() {
+        rainbowActivated = !rainbowActivated;
+
+        if(rainbowActivated) {
+            rainbowButton.classList.add("rainbowActive");
+
+        } else {
+            rainbowButton.classList.remove("rainbowActive");
+            rainbowButton.style.background = rainbowOldBG;
+        }
     })
 }
 
