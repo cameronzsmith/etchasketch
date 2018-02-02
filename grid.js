e06624f507d1a563d09163f2b0e332366abe5924
Cameron Smith <csmith158@butlercc.edu>
2018-02-01 19:39:02 -0600

    Functional grid with container that scales based on user input

--- grid.js
+++ grid.js
@@ -1,89 +1,52 @@
 createGrid(16); // Creates a blank 16x16 grid.
+_resetButtonClicked();
 
 // Creates a blank grid
 function createGrid(cell_size) {
-    let container = document.querySelector(".container");
 
-    // Adjust the grid based on the amount of cells provided to avoid having some cells larger than others.
-    if(cell_size < 21) {
-        container.style = "grid-template-columns: repeat(auto-fit, 30px);";
-    } else if(cell_size >= 21 && cell_size <= 28) {
-        container.style = "grid-template-columns: repeat(auto-fill, auto-fit);";
-    } else {
-        container.style = "grid-template-columns: repeat(auto-fit, auto-fit);";
-    }
+    let grid = document.querySelector(".grid");
+    let cell;
 
-    // Iterates through all the rows
-    for(let i = 0; i < cell_size; i++) {
-        let newRow = document.createElement("div");
-        
-        // Iterates through all the columns
-        for(let j = 0; j < cell_size; j++) {
-            let newCol = document.createElement("div");
-            newCol.classList.add("col");
+    for(let rows = 0; rows < cell_size; rows++) {
 
-            let cell = document.createElement("div");
+        for(let cols = 0; cols < cell_size; cols++) {
+            // Create new cell div and append the .cell class to the element.
+            cell = document.createElement("div");
             cell.classList.add("cell");
 
-            addHoverEffect(cell); // Appends the .active class to cells on hover.
+            // Sets cells to black on mouse over.
+            setActive(cell);
 
-            newRow.appendChild(newCol);
-            newCol.appendChild(cell);
+            // Add the cell to our grid.
+            grid.appendChild(cell);
         }
 
-        
-        newRow.classList.add("row");
-        container.appendChild(newRow);
     }
 
-    container.appendChild(createResetButton());
+    // Add a column to the grid-template-columns for every cell we have.
+    grid.style.setProperty('grid-template-columns', `repeat(${cell_size}, 1fr)`);
 }
 
 // Adds hover effect event listener for each cell.
-function addHoverEffect(cell) {
+function setActive(cell) {
     cell.addEventListener("mouseover", function() {
         cell.classList.add("active");
     })
 }
 
-// Creates a reset button
-function createResetButton() {
-    let resetButton = document.createElement("button");
-    resetButton.classList.add("resetButton");
-    resetButton.innerHTML = "Reset grid";
+function _resetButtonClicked() {
+    let resetButton = document.querySelector(".resetButton");
 
-    resetClickEvent(resetButton);
-
-    return resetButton;
-}
-
-// Handles the click event for the reset button
-function resetClickEvent(resetButton) {
     resetButton.addEventListener("click", function() {
-        let input = prompt("What size should the grid be?", 16);
-
+        let cellSize = prompt("Cell size", 16);    
         deleteGrid();
-        createGrid(input);
-
-        document.querySelectorAll(".active").forEach(function(element) {
-            element.classList.remove("active");
-        });
-    });
+        createGrid(cellSize);
+    })
 }
 
 // Deletes all of the elements contained within the grid.
 function deleteGrid() {
-    document.querySelectorAll(".row").forEach(function(element) {
-        element.remove();
-    });
-    
-    document.querySelectorAll(".col").forEach(function(element) {
-        element.remove();
-    });
-    
     document.querySelectorAll(".cell").forEach(function(element) {
         element.remove();
     });
-    
-    document.querySelector(".resetButton").remove();
 }
